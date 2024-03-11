27ca72b28739b951c58b65c8cc353a85300a82fc
Jaysen Hippensteel <105116606+jhippensteel@users.noreply.github.com>
2024-03-11 02:16:56 -0400

    Initial Commit - 3/11/2024

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <fstream>
+#include "clip/clip.h"
+
+using namespace std;
+
+int main() {
+    string action;
+    cin>>action;
+    fstream fio;
+    fio.open("pws.txt", ios::app | ios::out | ios::in);
+
+    if(action == "ENCRYPT"){
+        string plainText, key, application;
+        cout<<"Application:";
+        cin>>application;
+        cout<<"Plain Text:";
+        cin>>plainText;
+        cout<<"Key:";
+        cin>>key;
+        for(int i=1;i<=key.length();i++) {
+            for (int x = 1; x <= plainText.length(); x++) {
+                if(x>(i-1)*(plainText.length()/key.length()) & x<(i+1)*(plainText.length()/key.length())){
+                    if(int(plainText[x-1]) + int(key[i-1])<127){
+                        plainText[x-1] = char(int(plainText[x-1]) + int(key[i-1]));
+                    }
+                    else{
+                        plainText[x-1] = char(int(plainText[x-1]) + int(key[i-1])-95);
+                    }
+                }
+                if(i==key.length() & x>(i)*(plainText.length()/key.length())){
+                    if(int(plainText[x-1]) + int(key[i-1])<127){
+                        plainText[x-1] = char(int(plainText[x-1]) + int(key[i-1]));
+                    }
+                    else{
+                        plainText[x-1] = char(int(plainText[x-1]) + int(key[i-1])-95);
+                    }
+                }
+            }
+        }
+
+        fio<<application<<":"<<plainText<<endl;
+    }
+
+    if(action == "DECRYPT"){
+        string fileText, cryptText, key, application;
+        cryptText, application = "";
+        int c;
+        cout<<"Key:";
+        cin>>key;
+        getline(fio, fileText);
+
+        for(int i=0;i<fileText.length();i++){
+            if(fileText[i] == *":"){
+                c = i;
+                break;
+            }
+        }
+        for(int i=0;i<c;i++){application += fileText[i];}
+        for(int i=c+1;i<fileText.length();i++){cryptText += fileText[i];}
+
+        for(int i=1;i<=key.length();i++) {
+            for (int x = 1; x <= cryptText.length(); x++) {
+                if(x>(i-1)*(cryptText.length()/key.length()) & x<(i+1)*(cryptText.length()/key.length())){
+                    if(int(cryptText[x-1]) - int(key[i-1])>31){
+                        cryptText[x-1] = char(int(cryptText[x-1])-int(key[i-1]));
+                    }
+                    else{
+                        cryptText[x-1] = char(int(cryptText[x-1])-int(key[i-1])+95);
+                    }
+                }
+                if(i==key.length() & x>(i)*(cryptText.length()/key.length())){
+                    if(int(cryptText[x-1]) - int(key[i-1])>31){
+                        cryptText[x-1] = char(int(cryptText[x-1])-int(key[i-1]));
+                    }
+                    else{
+                        cryptText[x-1] = char(int(cryptText[x-1])-int(key[i-1])+95);
+                    }
+                }
+            }
+        }
+
+        clip::set_text(cryptText);
+    }
+
+    fio.close();
+    return 0;
+}
